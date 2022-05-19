abb71feefd123f52264b1a79d5c8d0099aa394c6
alexliou <alexliou0@gmail.com>
2022-05-19 15:27:09 -0400

    Initial Commit

---
+++ Challenge6/Challenge6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6App.swift
+//  Challenge6
+//
+//  Created by Alex Liou on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
